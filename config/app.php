5f1132cd3d74a68f09537d2b8872f82552850bc9
Benjamin Schuch <bschuch@gmx.net>
2016-05-14 03:30:52 +0200

    Removed some dev dependencies

--- config/app.php
+++ config/app.php
@@ -162,9 +162,9 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
-        Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
-        User11001\EloquentModelGenerator\EloquentModelGeneratorProvider::class,
+        //Way\Generators\GeneratorsServiceProvider::class,
+        //Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
+        //User11001\EloquentModelGenerator\EloquentModelGeneratorProvider::class,
     ],
 
     /*
