052ec5be1dfd3250838f02cf4b5e627576f28d63
Benjamin Schuch <bschuch@gmx.net>
2016-07-23 10:40:34 +0200

    Added new example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+class ExampleTest extends TestCase {
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->assertEquals(200, 200);
+    }
+}
